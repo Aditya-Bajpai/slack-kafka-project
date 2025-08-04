8884c41b2d460c62dd32db7aa045448cc08e2a35
Aditya Bajpai <bajpaiaditya356@gmail.com>
2025-08-04 19:04:14 +0530

    updated with auth protection

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,17 @@
 import requests
 from requests.auth import HTTPBasicAuth
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from tests.passwords import user, password
 
 node = "REMOTE_KAFKA"
 cluster = "cluster.5"
 broker = "1"
 
 url = f"http://129.80.133.91:8019/rest/v1/kafka/brokers/{node}/{cluster}/{broker}"
-auth = HTTPBasicAuth("Admin", "admin")
+auth = HTTPBasicAuth(user, password)
 
 try:
     res = requests.get(url, auth=auth)
